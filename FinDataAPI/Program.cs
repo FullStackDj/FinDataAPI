69256429e56c2c9a3d662f159c6f18e7d2fc74cc
FullStackDj <full.stackdj@gmail.com>
2025-06-05 10:13:44 +0300

    Added models, migrations and database connection

--- FinDataAPI/Program.cs
+++ FinDataAPI/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using FinDataAPI.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
