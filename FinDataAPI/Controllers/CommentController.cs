4862ad5c0e8c2c8e2c84cabc4b60ad1856b7a019
FullStackDj <full.stackdj@gmail.com>
2025-06-13 09:22:23 +0300

    Implemented delete endpoint to remove comments by ID

--- FinDataAPI/Controllers/CommentController.cs
+++ FinDataAPI/Controllers/CommentController.cs
@@ -67,4 +67,18 @@ public class CommentController : ControllerBase
 
         return Ok(comment.ToCommentDTO());
     }
+
+    [HttpDelete]
+    [Route("{id}")]
+    public async Task<IActionResult> Delete([FromRoute] int id)
+    {
+        var commentModel = await _commentRepo.DeleteAsync(id);
+
+        if (commentModel == null)
+        {
+            return NotFound("Comment don't exist");
+        }
+        
+        return Ok(commentModel);
+    }
 }
--- FinDataAPI/Repository/CommentRepository.cs
+++ FinDataAPI/Repository/CommentRepository.cs
@@ -46,4 +46,18 @@ public class CommentRepository : ICommentRepository
 
         return existingComment;
     }
+
+    public async Task<Comment?> DeleteAsync(int id)
+    {
+        var commentModel = await _context.Comments.FirstOrDefaultAsync(x => x.Id == id);
+
+        if (commentModel == null)
+        {
+            return null;
+        }
+        
+        _context.Comments.Remove(commentModel);
+        await _context.SaveChangesAsync();
+        return commentModel;
+    }
 }
